2c3987d2fa18b1b5b0016e7d53c633c2a13727d3
CarrieCanDo <carrie.a.wright@outlook.com>
2024-07-22 20:46:47 -0400

    committing printing assignment

---
+++ src/MyNameIs.java
@@ -0,0 +1,8 @@
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.print("My name is");
+        System.out.print("Carrie");
+        System.out.print("Wright");
+
+    }
+}
---
+++ src/Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.print("I'd like to visit");
+        System.out.print("Ireland");
+        System.out.print("Scotland");
+    }
+}
